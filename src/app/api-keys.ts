59176e73a46b85a1f18baa6f33326fb65d2a8088
alvin niamatali <aniamatali@icloud.com>
2018-05-15 09:53:17 -0700

    secret

---
+++ src/app/api-keys.ts
@@ -0,0 +1,7 @@
+export var masterFirebaseConfig = {
+    apiKey: "AIzaSyDw_Eun5RRYFuXTGEoSbljZIpIDgFB7CkE",
+    authDomain: "roster-tracker.firebaseapp.com",
+    databaseURL: "https://roster-tracker.firebaseio.com",
+    storageBucket: "roster-tracker.appspot.com",
+    messagingSenderId: "628208890767"
+  };
